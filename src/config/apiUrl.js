26be9f1eea260ea6ece7034915133e78019c4d91
pratik nikale <prnikale@gmail.com>
2023-06-20 19:18:37 +0530

    changed backend url from heroku to render

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,5 +1,7 @@
 export const local = "http://localhost:8000";
-export const live = "https://managefy-mern-app.herokuapp.com/";
+// export const live = "https://managefy-mern-app.herokuapp.com/";
+export const live = "https://managefy-managementapp.onrender.com";
 
 export const socketLocal = "http://localhost:8080";
-export const socketLive = "https://managefy-mern-app.herokuapp.com/";
+// export const socketLive = "https://managefy-mern-app.herokuapp.com/";
+export const socketLive = "https://managefy-managementapp.onrender.com";
